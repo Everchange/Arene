b63166e684db019bed2ed39ddfc91537130cd7cc
Altaren <clement.maps@gmail.com>
2016-12-27 22:14:24 +0100

    Update Main.java

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -1,38 +1,41 @@
-package graphics;
-
 import javafx.application.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.*;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
-import javafx.stage.*;
+import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        // définit la largeur et la hauteur de la fenêtre
-        // en pixels, le (0, 0) se situe en haut à gauche de la fenêtre
+	@Override
+	public void start(Stage stage) throws Exception {
+		// dÃ©finit la largeur et la hauteur de la fenÃªtre
+        // en pixels, le (0, 0) se situe en haut Ã  gauche de la fenÃªtre
         stage.setWidth(800);
         stage.setHeight(600);
-        // met un titre dans la fenêtre
+        // met un titre dans la fenÃªtre
         stage.setTitle("Truc");
 
         // la racine du sceneGraph est le root
         Group root = new Group();
         
-        
+        // load the image for the background
         Image image = new Image("file:///D:/workspace/eclipse/Game (Merlin)/data/field.png");
         ImageView imageView = new ImageView();
         imageView.setImage(image);
+        // add the image to the root
         root.getChildren().add(imageView);
         
+        //(Rage)Quit button
         Button btn = new Button();
         btn.setText("Quit");
+        //defines the action when the button is pressed
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
@@ -43,13 +46,13 @@ public class Main extends Application {
         
         root.getChildren().add(btn);
         
+        //set the scene
         Scene scene = new Scene(root);
-        scene.setFill(Color.GREY);       
-        
+        scene.setFill(Color.GREY);
         stage.setScene(scene);
         // ouvrir le rideau
         stage.show();
 
-    }
+	}
 
 }

b01f6563519277476aad85d17a7730a784ddca5c
Everchange <mdumeur@gmail.com>
2016-12-28 00:30:35 +0100

    simplification

--- src/Armor.java
+++ src/Armor.java
@@ -1,82 +1,38 @@
 public class Armor {
 
-	private int CA, maxDex, weight;
-	protected String RIEN="Aucune armure", MATELASSEE="Armure matelassée", CUIR="Armure de cuir", PEAU="Armure de peau", CLOUTE="Armure de cuir cloute", CHEMISE="Chemise de mailles", COTTE="Cotte de mailles", CUIRASSE="Cuirasse", PLAQUE="Armure de plaques";
+	private int CA, maxDex, dexBonus, weight;
+	private String nom;
+	protected String[] noms = {"Aucune armure", "Armure matelassée", "Armure de cuir", "Armure de peau", "Armure de cuir cloute", "Chemise de mailles", "Cotte de mailles", "Cuirasse", "Armure de plaques"};
+	private int[] CAs = {10,11,12,13,14,15,16,17,18};
+	private int[] mDs = {-1,4,4,3,3,2,2,1,0};
+	private int[] weights = {0,1,1,1,2,2,2,3,3};
 	
-	public Armor(int nom, int dxmod) {
+	public Armor(int modele, int dxmod) {
 		
-		if (nom == 0){
-			
-			this.CA = 10;
-			this.maxDex = -1;
-			this.weight = 0;
-			
-		}
-		else if (nom == 1) {
-			
-			this.CA = 11;
-			this.maxDex = 4;
-			this.weight = 1;
-			
-		}
-		else if (nom == 2) {
-			
-			this.CA = 12;
-			this.maxDex = 4;
-			this.weight = 1;
-			
-		}
-		else if (nom == 3) {
-			
-			this.CA = 13;
-			this.maxDex = 3;
-			this.weight = 1;
-			
-		}
-		else if (nom == 4) {
-			
-			this.CA = 14;
-			this.maxDex = 3;
-			this.weight = 2;
-			
-		}
-		else if (nom == 5) {
-			
-			this.CA = 15;
-			this.maxDex = 2;
-			this.weight = 2;
-			
-		}
-		else if (nom == 6) {
-			
-			this.CA = 16;
-			this.maxDex = 2;
-			this.weight = 2;
-			
-		}
-		else if (nom == 7) {
-			
-			this.CA = 17;
-			this.maxDex = 1;
-			this.weight = 3;
-			
-		}
-		else {
-			
-			this.CA = 18;
-			this.maxDex = 0;
-			this.weight = 3;
-			
-		}
+		this.CA = CAs[modele];
+		this.maxDex = mDs[modele];
+		this.weight = weights[modele];
+		this.nom = noms[modele];
 		
-		this.maxDex = Math.min(dxmod, this.maxDex);
-		
+		this.dexBonus = Math.min(dxmod, this.maxDex);
 		
 	}
 	
 	public boolean hit (int touche) {
 		
-		return (touche > (CA+this.maxDex));
+		return (touche > (this.CA+this.dexBonus));
+		
+	}
+	
+	public String getNom() {
+		
+		return this.nom;
+		
+	}
+	
+	public int getCA(){
+		
+		return (this.CA+this.dexBonus);
 		
 	}
 	

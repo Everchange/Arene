cfc443d60be6bc7528d8d2a7f0ee406df9ae9263
Everchange <mdumeur@gmail.com>
2016-12-27 21:49:36 +0100

    Fenetre Clement

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -1,18 +1,50 @@
 package graphics;
 
 import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.*;
 
 
-public class Main extends Application{
-
-    public static void main(String[] args) {
-        System.out.println( "Main method inside Thread : " +  Thread.currentThread().getName());
-        launch(args);
-    }
+public class Main extends Application {
 
     @Override
-    public void start(Stage args) throws Exception {
-        System.out.println( "Start method inside Thread : " +  Thread.currentThread().getName());
+    public void start(Stage stage) throws Exception {
+        // définit la largeur et la hauteur de la fenêtre
+        // en pixels, le (0, 0) se situe en haut à gauche de la fenêtre
+        stage.setWidth(800);
+        stage.setHeight(600);
+        // met un titre dans la fenêtre
+        stage.setTitle("Joli décor!");
+
+        // la racine du sceneGraph est le root
+        Group root = new Group();
+        
+        
+        Image image = new Image("file:///D:/workspace/eclipse/Game (Merlin)/data/field.png");
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+        root.getChildren().add(imageView);
+        
+        Scene scene = new Scene(root);
+        
+        
+              
+        
+        scene.setFill(Color.GREY);
+        
+       
+        
+        stage.setScene(scene);
+        // ouvrir le rideau
+        stage.show();
+
     }
+
 }

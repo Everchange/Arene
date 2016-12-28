19fec6b55fd34286468c6138002ccf72fa15e604
Clément <clement.maps@gmail.com>
2016-12-28 11:28:28 +0100

    petits réglages

--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage stage) throws Exception {
 		// définit la largeur et la hauteur de la fenêtre
         // en pixels, le (0, 0) se situe en haut à gauche de la fenêtre
-        stage.setWidth(735);
+        stage.setWidth(800);
         stage.setHeight(600);
         // met un titre dans la fenêtre
         stage.setTitle("Truc");

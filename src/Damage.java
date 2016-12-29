55008aa5b4ac68472ea15ed4879e5e19fe548cf4
Everchange <mdumeur@gmail.com>
2016-12-29 13:54:28 +0100

    Commencé les armes et les dégâts

--- src/Damage.java
+++ src/Damage.java
@@ -1,22 +1,43 @@
-
+import java.util.Random;
 
 public class Damage {
 	
 	private int bonus, nombreDes, nombreFaces;
+	private Random RNG;
 	
 	public Damage(int b, int nd, int nf) {
 		
 		this.bonus = b;
-		this.nombreDes = nd;
 		this.nombreFaces = nf;
+		this.nombreDes = nd;
 		
 	}
 	
+	public int degats(int aAjouter) {
+		
+		int resultat = 0;
+		for (int i=0 ; i<this.nombreDes ; i++) {
+			
+			resultat += RNG.nextInt(this.nombreFaces)+1;
+			
+		}
+		resultat += this.bonus;
+		resultat += aAjouter;
+		return resultat;
+				
+		
+	}
 	
-//	public int roll(int boni) {
-//		
-//		return 
-//		
-//	}
-
+	public int crit(int aAjouter, int multiplicateur){
+		
+		int resultat = 0;
+		for (int i = 0 ; i<multiplicateur ; i++) {
+			
+			resultat += this.degats(aAjouter);
+			
+		}
+		
+		return resultat;
+		
+	}
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -5,23 +5,36 @@ public abstract class Weapon {
 	private int bonusToucher;
 	private Damage degats;
 	private int critMultiplicateur;
+	private int critRange;
+	private boolean ranged;
+	private String nom;
 	
-	public Weapon(String nom) {
+	private int[] critMultiplicateurs = {2,3,2,2,2,3,2,3,2,3,3};
+	private int[] critRanges = {2,1,2,1,2,1,2,1,1,1,1};
+	private String[] nomsArmes = {"Epee à 2 mains", "Grande hache", "Fléau lourd", "Massue", "Epee longue", "Hache d'armes", "Epee courte", "Hachette", "Marteau léger", "Arc court", "Arc long"};
+	private boolean[] ranges = {false, false, false, false, false, false, false, false, false, true, true};
+			
+	public Weapon(int pos) {
 		
-		if (nom == "hache") {
-			
-			bonusToucher = 42;
-			degats = new Damage(1,2,3);
-			critMultiplicateur = 14;
-			
-		}
+		this.critMultiplicateur = this.critMultiplicateurs[pos];
+		this.critRange = this.critRanges[pos];
+		this.ranged = this.ranges[pos];
+		this.nom = this.nomsArmes[pos];
 		
 	}
 	
-//	public static int attaque(int bt, int bd) {
-//		
-//		if 
-//		
-//	}
-
+	public String getName() {
+		
+		return this.nom;
+		
+	}
+	
+	public boolean aDistance(){
+		
+		return this.ranged;
+		
+	}
+	
+	
+	
 }

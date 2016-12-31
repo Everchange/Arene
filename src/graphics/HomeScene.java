c1e86e8ae0ee44d85293f7b59d50e3acb4caa84f
Clément <clement.maps@gmail.com>
2016-12-31 09:43:50 +0100

    new scene
    > added the home scene class

---
+++ src/graphics/HomeScene.java
@@ -0,0 +1,18 @@
+package graphics;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+public class HomeScene {
+	
+	private Scene scene;
+	private Group root=new Group();
+	
+	public HomeScene(){
+		
+	}
+	
+	public Scene getScene(){
+		return this.scene;
+	}
+}
